265d182501d17013089ab1b11a5f4db0cdaece70
bilal07 <mouadine.bilal@gmail.com>
2021-01-29 23:07:39 +0100

    list items component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div className="App">
          TodoList App
-         <TodoItems />
+         <TodoItems items={this.state.items}/>
          <AddItems />
       </div>
     );
--- src/components/TodoItems/TodoItems.js
+++ src/components/TodoItems/TodoItems.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './TodoItems.css';
 
-const TodoItems = () => {
+const TodoItems = (props) => {
+    const items = props.items;
+    const listItems = items.map(item => {
+        return(
+            <div key={item.id}>
+                <span>{item.name}</span>
+                <span>{item.age}</span>
+                <span>&times;</span>
+            </div>
+        )
+    })
     return (
-        <div>
-            List Items
+        <div className="ListItems">
+            <div>
+                <span>Name</span>
+                <span>Age</span>
+                <span>Action</span>
+            </div>
+            {listItems}
         </div>
     )
     }
