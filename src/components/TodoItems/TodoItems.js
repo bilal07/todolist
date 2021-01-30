108e224721d693c8d0f19f9a5120175680fc617e
bilal07 <mouadine.bilal@gmail.com>
2021-01-30 20:29:23 +0100

    show no item message

--- src/components/TodoItems/TodoItems.js
+++ src/components/TodoItems/TodoItems.js
@@ -4,15 +4,21 @@ import './TodoItems.css';
 const TodoItems = (props) => {
     const items = props.items;
     const deleteItem = props.deleteItem;
-    const listItems = items.map(item => {
-        return(
-            <div key={item.id}>
-                <span>{item.name}</span>
-                <span>{item.age}</span>
-                <span onClick={() => deleteItem(item.id)}>&times;</span>
-            </div>
-        )
-    })
+    let length = items.length;
+    const listItems = length ?(
+        items.map(item => {
+            return(
+                <div key={item.id}>
+                    <span>{item.name}</span>
+                    <span>{item.age}</span>
+                    <span onClick={() => deleteItem(item.id)}>&times;</span>
+                </div>
+            )
+        })
+    ) : (
+        <p>There is no item to show</p>
+    )
+    
     return (
         <div className="ListItems">
             <div>
